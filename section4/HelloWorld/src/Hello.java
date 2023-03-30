074a4797052a3f50ae065193d22867b68bd0c6d6
romuloabbiati <rhabbiati@hotmail.com>
2023-03-30 04:58:36 +0100

    Assignment operator vs equals to operator

--- section4/HelloWorld/src/Hello.java
+++ section4/HelloWorld/src/Hello.java
@@ -22,6 +22,16 @@ public class Hello {
         if((topScore > 90) || (secondTopScore <= 90)) {
             System.out.println("Either or both of the conditions are true");
         }
+
+        int newValue = 50;
+        if(newValue == 50) {
+            System.out.println("This is an error");
+        }
+
+        boolean isCar = false;
+        if(!isCar) {
+            System.out.println("This is not supposed to happen");
+        }
     }
 
 }
