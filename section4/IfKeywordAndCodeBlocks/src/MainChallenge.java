d5fedaafb43f4f74606ff70c057f7711c3291a17
romuloabbiati <rhabbiati@hotmail.com>
2023-04-02 12:00:41 +0100

    if then else challenge

---
+++ section4/IfKeywordAndCodeBlocks/src/MainChallenge.java
@@ -0,0 +1,30 @@
+public class MainChallenge {
+
+    public static void main(String[] args) {
+
+        boolean gameOver = true;
+        int score = 800;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+        int finalScore = score;
+
+        if(gameOver == true) {
+            finalScore += (levelCompleted * bonus);
+            System.out.println("Your final score was " + finalScore);
+        }
+
+        score = 10000;
+        levelCompleted = 8;
+        bonus = 200;
+
+        finalScore = score;
+
+        if(gameOver == true) {
+            finalScore += (levelCompleted * bonus);
+            System.out.println("Your final score was " + finalScore);
+        }
+
+    }
+
+}
