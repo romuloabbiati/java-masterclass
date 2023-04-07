646f898bea40c9f5e54aaf461d30e07513633d2c
romuloabbiati <rhabbiati@hotmail.com>
2023-04-07 07:15:48 +0100

    Tradicional & enhanced switch statement

---
+++ section6/Switch/src/Main.java
@@ -0,0 +1,75 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+//        int value = 7;
+//        if(value == 1) {
+//            System.out.println("Value was 1");
+//        } else if(value == 2) {
+//            System.out.println("Value was 2");
+//        } else {
+//            System.out.println("Was not 1 or 2");
+//        }
+
+        int switchValue = 3;
+
+        switch(switchValue) {
+            case 1:
+                System.out.println("Value was 1");
+                break;
+            case 2:
+                System.out.println("Value was 2");
+                break;
+            case 3: case 4: case 5:
+                System.out.println("Was a 3, a 4, or a 5");
+                System.out.println("Actually it was a " + switchValue);
+                break;
+            default:
+                System.out.println("Was not 1 or 2");
+                break;
+        }
+
+        String month = "APRIL";
+        System.out.println(month + " is in the " + getQuarter(month) + " quarter");
+        System.out.println();
+        month = "XYZ";
+        System.out.println(month + " is in the " + getQuarterEnhanced(month) + " quarter");
+
+    }
+
+    public static String getQuarter(String month) {
+        switch(month) {
+            case "JANUARY":
+            case "FEBRUARY":
+            case "MARCH":
+                return "1st";
+            case "APRIL":
+            case "MAY":
+            case "JUNE":
+                return "2nd";
+            case "JULY":
+            case "AUGUST":
+            case "SEPTEMBER":
+                return "3rd";
+            case "OCTOBER":
+            case "NOVEMBER":
+            case "DECEMBER":
+                return "4th";
+        }
+        return "bad";
+    }
+
+    public static String getQuarterEnhanced(String month) {
+        return switch(month) {
+            case "JANUARY", "FEBRUARY", "MARCH" -> "1st";
+            case "APRIL", "MAY", "JUNE" -> "2nd";
+            case "JULY", "AUGUST", "SEPTEMBER" -> "3rd";
+            case "OCTOBER", "NOVEMBER", "DECEMBER" -> "4th";
+            default -> {
+                String badResponse = month + " is bad";
+                yield badResponse;
+            }
+        };
+    }
+
+}
