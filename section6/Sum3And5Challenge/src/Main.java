bf43cd824dc54133c3a5bccf6aadd3795b1960a3
romuloabbiati <rhabbiati@hotmail.com>
2023-04-08 11:48:15 +0100

    Sum3And5Challenge

---
+++ section6/Sum3And5Challenge/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int count = 0;
+        int sum = 0;
+
+        for(int i = 1; i <= 1000; i++) {
+            if((i % 3 == 0) && (i % 5 == 0)) {
+                sum += i;
+                count++;
+                System.out.println(i + " is divisible by 3 and 5");
+            }
+            if(count == 5) {
+                break;
+            }
+        }
+        System.out.println("Sum of numbers that are divisible by 3 and 5 = " + sum);
+    }
+
+}
