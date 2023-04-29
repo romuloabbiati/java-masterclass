03e4beb9cbda625afcd9192454ee005809b3db65
romuloabbiati <rhabbiati@hotmail.com>
2023-04-29 11:26:15 +0100

    Casting with classes, using Object and var references

---
+++ section8/Polymorphism/src/NextMain.java
@@ -0,0 +1,23 @@
+public class NextMain {
+
+    public static void main(String[] args) {
+
+        Movie movie = Movie.getMovie("A", "Jaws");
+        movie.watchMovie();
+
+        Adventure jaws = (Adventure) Movie.getMovie("A", "Jaws");
+        jaws.watchMovie();
+
+        Object comedy = Movie.getMovie("C", "National Lampoon's Day");
+//        comedy.watchMovie();
+        Comedy comedyMovie = (Comedy) comedy;
+        comedyMovie.watchComedy();
+
+        var airplane = Movie.getMovie("Comedy", "Airplane");
+        airplane.watchMovie();
+
+        var plane = new Comedy("Airplane");
+        plane.watchComedy();
+    }
+
+}
