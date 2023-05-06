74fd1428e1521fbfe3bdb99856aefe7a0e4da9bc
romuloabbiati <rhabbiati@hotmail.com>
2023-05-06 09:43:41 +0100

    Organizing Java Classes, Packages and Import Statements

---
+++ section8/Packages/src/com/smartgroup/Main.java
@@ -0,0 +1,14 @@
+package com.smartgroup;
+
+import dev.lpa.first.Item;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Item firstItem = new Item("Burger");
+        System.out.println(firstItem);
+
+    }
+
+}
---
+++ section8/Packages/src/dev/lpa/first/Item.java
@@ -0,0 +1,17 @@
+package dev.lpa.first;
+
+public class Item {
+
+    private String name;
+
+    public Item(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
