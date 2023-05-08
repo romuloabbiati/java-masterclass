0904a137896b1fde6e37c1d3c236f303491a6eab
romuloabbiati <rhabbiati@hotmail.com>
2023-05-08 12:27:46 +0100

    Arrays Part 2

--- section9/ArraysProject/src/com/smartgroup/Main.java
+++ section9/ArraysProject/src/com/smartgroup/Main.java
@@ -1,5 +1,7 @@
 package com.smartgroup;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -21,10 +23,31 @@ public class Main {
 
         int[] newArray;
 //        newArray = {5, 4, 3, 2, 1}; array initializer is not allowed outside of the declaration statement
-        newArray = new int[] {5, 4, 3, 2, 1};
+//        newArray = new int[] {5, 4, 3, 2, 1};
+        newArray = new int[5];
+        for(int i = 0; i < newArray.length; i++) {
+            newArray[i] = newArray.length - i;
+        }
+
         for(int i = 0; i < newArray.length; i++) {
             System.out.print(newArray[i] + " ");
         }
+        System.out.println();
+        for(int element : newArray) {
+            System.out.print(element + " ");
+        }
+        System.out.println();
+        System.out.println(Arrays.toString(newArray)); // newArray = [5, 4, 3, 2, 1]
+
+        Object objectVariable = newArray;
+        if(objectVariable instanceof int[]) {
+            System.out.println("objectVariable is really an int array");
+        }
+
+        Object[] objectArray = new Object[3];
+        objectArray[0] = "Hello";
+        objectArray[1] = new StringBuilder("World");
+        objectArray[2] = newArray;
 
     }
 
