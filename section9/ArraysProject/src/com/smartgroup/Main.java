681e5e2be51573be0bdc4f42f40aef45856c0c7e
romuloabbiati <rhabbiati@hotmail.com>
2023-05-08 10:40:20 +0100

    Arrays Part 1

---
+++ section9/ArraysProject/src/com/smartgroup/Main.java
@@ -0,0 +1,31 @@
+package com.smartgroup;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[] myIntArray = new int[10];
+        myIntArray[0] = 45;
+        myIntArray[1] = 0;
+        myIntArray[5] = 50;
+
+        double[] myDoubleArray = new double[10];
+        myDoubleArray[2] = 3.5;
+        System.out.println(myDoubleArray[2]);
+
+        int[] firstTen = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        System.out.println("first = " + firstTen[0]);
+        int arrayLength = firstTen.length;
+        System.out.println("length of array = " + arrayLength);
+        System.out.println("last = " + firstTen[arrayLength - 1]);
+
+        int[] newArray;
+//        newArray = {5, 4, 3, 2, 1}; array initializer is not allowed outside of the declaration statement
+        newArray = new int[] {5, 4, 3, 2, 1};
+        for(int i = 0; i < newArray.length; i++) {
+            System.out.print(newArray[i] + " ");
+        }
+
+    }
+
+}
