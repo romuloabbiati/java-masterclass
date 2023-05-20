dda51fb79d50d9a31c22f5592eaaa43718fd15b6
romuloabbiati <rhabbiati@hotmail.com>
2023-05-20 20:12:30 +0100

    ArrayList Challenge Part 1

---
+++ section10/ArrayAndArrayLists/src/com/smartgroup/Main.java
@@ -0,0 +1,29 @@
+package com.smartgroup;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String[] originalArray = new String[] {"First", "Second", "Third"};
+        var originalList = Arrays.asList(originalArray);
+
+        originalList.set(0, "one");
+//        originalArray[0] = "one";
+        System.out.println("list: " + originalList);
+        System.out.println("array: " + Arrays.toString(originalArray));
+
+        originalList.sort(Comparator.naturalOrder());
+        System.out.println(Arrays.toString(originalArray));
+//        originalList.remove(0);
+
+//        originalList.add("fourth");
+
+        List<String> newList = Arrays.asList("Sunday", "Monday", "Tuesday");
+        System.out.println(newList);
+    }
+
+}
