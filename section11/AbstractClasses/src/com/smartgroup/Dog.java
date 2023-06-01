718077abb3cd9b7a666b7d874c8b1b6b28249416
romuloabbiati <rhabbiati@hotmail.com>
2023-06-01 07:54:54 +0100

    Abstract Classes Part1

---
+++ section11/AbstractClasses/src/com/smartgroup/Dog.java
@@ -0,0 +1,18 @@
+package com.smartgroup;
+
+public class Dog extends Animal {
+
+    public Dog(String type, String size, double weight) {
+        super(type, size, weight);
+    }
+
+    @Override
+    public void move(String speed) {
+
+    }
+
+    @Override
+    public void makeNoise() {
+
+    }
+}
---
+++ section11/AbstractClasses/src/com/smartgroup/Main.java
@@ -0,0 +1,11 @@
+package com.smartgroup;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+}
