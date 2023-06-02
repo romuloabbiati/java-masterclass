adb8e45a880815f8ffaefafabaa7553ac4dde052
romuloabbiati <rhabbiati@hotmail.com>
2023-06-02 05:39:40 +0100

    Abstract class part 2

--- section11/AbstractClasses/src/com/smartgroup/Dog.java
+++ section11/AbstractClasses/src/com/smartgroup/Dog.java
@@ -8,11 +8,19 @@ public class Dog extends Animal {
 
     @Override
     public void move(String speed) {
-
+        if(speed.equals("slow")) {
+            System.out.println(type + " walking");
+        } else {
+            System.out.println(type + " running");
+        }
     }
 
     @Override
     public void makeNoise() {
-
+        if(type == "Wolf") {
+            System.out.print("Howling! ");
+        } else {
+            System.out.print("Woof! ");
+        }
     }
 }
--- section11/AbstractClasses/src/com/smartgroup/Main.java
+++ section11/AbstractClasses/src/com/smartgroup/Main.java
@@ -1,11 +1,31 @@
 package com.smartgroup;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        Dog dog = new Dog("Wolf", "big", 100.00);
+        dog.makeNoise();
+        doAnimalStuff(dog);
 
+        ArrayList<Animal> animals = new ArrayList<>();
+        animals.add(dog);
+        animals.add(new Dog("German Shephard", "big", 150.00));
+        animals.add(new Fish("Goldfish", "small", 1.00));
+        animals.add(new Fish("Barracuda", "big", 75.00));
+        animals.add(new Fish("Pug", "small", 20.00));
 
+        for(Animal animal : animals) {
+            doAnimalStuff(animal);
+        }
+
+    }
+
+    public static void doAnimalStuff(Animal animal) {
+        animal.makeNoise();
+        animal.move("slow");
     }
 
 }
