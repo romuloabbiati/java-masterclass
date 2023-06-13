438a1d5f78ca648633e0cc2160b5ecef444f834b
romuloabbiati <rhabbiati@hotmail.com>
2023-06-13 07:33:10 +0100

    Interfaces part3

--- section11/Interfaces/src/com/smartgroup/Animal.java
+++ section11/Interfaces/src/com/smartgroup/Animal.java
@@ -1,5 +1,60 @@
 package com.smartgroup;
 
+enum FlightStages implements Trackable {GROUNDED, LAUNCH, CRUISE, DATA_COLLECTION;
+
+    @Override
+    public void track() {
+        if(this != GROUNDED) {
+            System.out.println("Monitoring " + this);
+        }
+    }
+}
+
+record DragonFly(String name, String type) implements FlightEnabled {
+
+    @Override
+    public void takeOff() {
+
+    }
+
+    @Override
+    public void land() {
+
+    }
+
+    @Override
+    public void fly() {
+
+    }
+}
+
+class Satellite implements OrbitEarth {
+
+    public void achieveOrbit() {
+        System.out.println("Orbit achieved!");
+    }
+
+    @Override
+    public void takeOff() {
+
+    }
+
+    @Override
+    public void land() {
+
+    }
+
+    @Override
+    public void fly() {
+
+    }
+}
+
+interface OrbitEarth extends FlightEnabled {
+
+    void achieveOrbit();
+}
+
 interface FlightEnabled {
 
     double MILES_TO_KM = 1.60934;
--- section11/Interfaces/src/com/smartgroup/Main.java
+++ section11/Interfaces/src/com/smartgroup/Main.java
@@ -1,5 +1,9 @@
 package com.smartgroup;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -26,6 +30,22 @@ public class Main {
         double kmsTravelled = 100.0;
         double milesTravelled = kmsTravelled * FlightEnabled.MILES_TO_KM;
         System.out.printf("The truck travelled  %.2f km or %.2f miles%n", kmsTravelled, milesTravelled);
+
+//        ArrayList<FlightEnabled> fliers = new ArrayList<>();
+//        fliers.add(bird);
+        LinkedList<FlightEnabled> fliers = new LinkedList<>();
+        fliers.add(bird);
+
+        List<FlightEnabled> betterFliers = new ArrayList<>();
+        betterFliers.add(bird);
+
+        triggerFliers(fliers);
+        flyFliers(fliers);
+        landFliers(fliers);
+
+        triggerFliers(betterFliers);
+        flyFliers(betterFliers);
+        landFliers(betterFliers);
     }
 
     private static void inFlight(FlightEnabled flier) {
@@ -35,6 +55,24 @@ public class Main {
             tracked.track();
         }
         flier.land();
+    }
+
+    private static void triggerFliers(ArrayList<FlightEnabled> fliers) {
+        for(var flier : fliers) {
+            flier.takeOff();
+        }
+    }
+
+    private static void flyFliers(ArrayList<FlightEnabled> fliers) {
+        for(var flier : fliers) {
+            flier.fly();
+        }
+    }
+
+    private static void landFliers(ArrayList<FlightEnabled> fliers) {
+        for(var flier : fliers) {
+            flier.land();
+        }
     }
 
 }
